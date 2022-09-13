a8c70e2ee08f460313d70557563ac714b9596a1f
thviet84 <viethoangtv84@gmail.com>
2022-09-13 17:29:48 +0700

    Define routes for managing contacts

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express'); 
 const cors = require('cors');
+const contactController = require('./controllers/contact.controller');
 
 const app = express();
 
@@ -10,4 +11,17 @@ app.get('/', (req, res) => {
     res.json({ message: 'Welcome to contact book application.' });
 });
 
+app.route('/api/contacts')
+    .get(contactController.findAll) 
+    .post(contactController.create) 
+    .delete(contactController.deleteAll);
+
+app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
+
+
+app.route('/api/contacts/:id')
+    .get(contactController.findOne) 
+    .put(contactController.update) 
+    .delete(contactController.delete);
+    
 module.exports = app;
